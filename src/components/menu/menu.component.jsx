07c6407f51344b126f82982a2bd77a37a98b8539
zaidsidd360 <zaidsidd360@gmail.com>
2022-10-03 01:46:34 +0530

    Added story count on top right and fixed some style issues

--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -1,43 +1,46 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import Button from "../button/button.component"
+import Button from "../button/button.component";
 
 import "./menu.styles.css";
 
-function Menu({ setIsNewStory }) {
+function Menu({ setIsNewStory, visibleStoriesCount }) {
   const [activeBtn, setActiveBtn] = useState("new");
 
   const handleActiveBtn = (btn) => {
     setActiveBtn(btn);
-    
+
     if (btn === "new") {
       setIsNewStory(true);
     } else if (btn === "past") {
       setIsNewStory(false);
     }
-  }
+  };
 
   return (
     <section data-testid="menu" className="menu-bar">
-      <Button
-        type="new"
-        width="61px"
-        height="auto"
-        isActive={activeBtn === "new"}
-        handleBtnClick={handleActiveBtn}
-      >
-        New
-      </Button>
+      <div>
+        <Button
+          type="new"
+          width="61px"
+          height="auto"
+          isActive={activeBtn === "new"}
+          handleBtnClick={handleActiveBtn}
+        >
+          New
+        </Button>
 
-      <Button
-        type="past"
-        width="61px"
-        height="auto"
-        isActive={activeBtn === "past"}
-        handleBtnClick={handleActiveBtn}
-      >
-        Best
-      </Button>
+        <Button
+          type="past"
+          width="61px"
+          height="auto"
+          isActive={activeBtn === "past"}
+          handleBtnClick={handleActiveBtn}
+        >
+          Best
+        </Button>
+      </div>
+      <h1 className="stories-count">Stories count: {visibleStoriesCount}</h1>
     </section>
   );
 }
