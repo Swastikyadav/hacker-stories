e6e5b5ffcaf221bfdc84800dfd6210c7639d3b6f
Tusharjain123 <jain0735@gmail.com>
2022-10-05 08:46:48 +0530

    Added 404 not found page

---
+++ src/components/404/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import image from "../../assets/404_page.png"
+import "./error.css"
+
+export default function Error() {
+  return (
+    <div className='image'>
+      <img src={image} alt="" />
+    </div>
+  )
+}
